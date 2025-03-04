28cc03f0792fb2afbb6aad789d5b6d3282d071b1
Hugo Almeida <hugo.almeida@boldlink.io>
2025-03-04 12:57:39 +0000

    release: adding the correct crawler variable using optional

--- modules/crawler/variables.tf
+++ modules/crawler/variables.tf
@@ -13,37 +13,30 @@ variable "crawlers" {
     schema_change_policy = optional(object({
       delete_behavior = optional(string) # Valid values: LOG, DELETE_FROM_DATABASE, or DEPRECATE_IN_DATABASE
       update_behavior = optional(string) # Valid values: LOG or UPDATE_IN_DATABASE
-      }), {
-      delete_behavior = "LOG"
-      update_behavior = "UPDATE_IN_DATABASE"
-    })
+    }))
     targets = optional(object({
       s3_targets = optional(list(object({
         path       = string
-        exclusions = optional(list(string), [])
-      })), [])
+        exclusions = optional(list(string))
+      })))
       jdbc_targets = optional(list(object({
         connection_name = string
         path            = string
-        exclusions      = optional(list(string), [])
-      })), [])
+        exclusions      = optional(list(string))
+      })))
       dynamo_db_targets = optional(list(object({
         path = string
-      })), [])
+      })))
       catalog_targets = optional(list(object({
         database_name = string
         table_name    = optional(string)
-      })), [])
-      }), {
-      s3_targets        = []
-      jdbc_targets      = []
-      dynamo_db_targets = []
-      catalog_targets   = []
-    })
+      })))
+    }))
     tags = optional(map(string))
   }))
   default = {}
 }
+
 
 variable "default_s3_target" {
   description = "Default S3 target path to use if no target is specified for a crawler."
