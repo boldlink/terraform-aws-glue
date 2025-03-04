537ed09647ccfce3c3683236b1f592eb2288d0df
Hugo Almeida <hugo.almeida@boldlink.io>
2025-03-04 10:25:36 +0000

    release: adding the description to the outputs

--- modules/global/outputs.tf
+++ modules/global/outputs.tf
@@ -1,7 +1,9 @@
 output "glue_encryption_settings" {
   value = aws_glue_data_catalog_encryption_settings.main.*.id
+  description = "The ID of the Glue Data Catalog encryption settings"
 }
 
 output "glue_resource_policy" {
   value = aws_glue_resource_policy.this.*.policy
+  description = "The Glue resource policy"
 }
