e9a290f2bdbab26ff433178c94d2c655c3bce1cd
Hugo Almeida <hugo.almeida@boldlink.io>
2025-03-04 16:17:59 +0000

    release: adding the previous variables

--- modules/catalog/variables.tf
+++ modules/catalog/variables.tf
@@ -2,24 +2,24 @@ variable "catalogs" {
   description = "Map of Glue catalog database configurations. Only the catalog name is required; all other fields are optional."
   type = map(object({
     name        = string
-    description = optional(string)
-    parameters  = optional(map(string))
+    description = optional(string, "")
+    parameters  = optional(map(string), {})
     federated_database = optional(object({
       connection_name = string
       identifier      = string
-    }))
+    }), null)
     target_database = optional(object({
       catalog_id    = string
       database_name = string
       region        = string
-    }))
+    }), null)
     create_table_default_permission = optional(object({
       permissions = list(string)
       principal = object({
         data_lake_principal_identifier = string
       })
-    }))
-    tags = optional(map(string))
+    }), null)
+    tags = optional(map(string), {})
   }))
   default = {}
 }
--- modules/catalog/versions.tf
+++ modules/catalog/versions.tf
@@ -1,6 +1,5 @@
 terraform {
   required_version = ">= 0.14.11"
-  experiments = [module_variable_optional_attrs]
   required_providers {
     aws = {
       source  = "hashicorp/aws"
